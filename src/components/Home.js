d30502353630f29922330593ef15e0a45382e65a
sandysams0n <69503452+sandysams0n@users.noreply.github.com>
2022-02-06 13:58:47 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+class Home extends React.Component{
+    render()
+        {
+            return(
+                <div>
+
+                </div>
+                )
+            
+        }
+
+}
+
+//ES5
+function Home(){
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+//ES6
+const Home = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
